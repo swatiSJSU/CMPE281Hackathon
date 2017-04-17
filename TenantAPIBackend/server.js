696b2059ae3a9a6208c36b30d1b6d45cb8888f94
swatiSJSU <swati.gupta@sjsu.edu>
2017-04-17 10:45:28 -0700

    Update server.js

--- TenantAPIBackend/server.js
+++ TenantAPIBackend/server.js
@@ -28,6 +28,13 @@ var router = express.Router();              // get an instance of the express Ro
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
+    //Access Control
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//and remove cacheing so we get the most recent comments
+ res.setHeader('Cache-Control', 'no-cache');
     // do logging
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
